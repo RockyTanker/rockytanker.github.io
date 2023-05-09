91eb0a47e33848949e144d54ec0f26e8f914f3fa
ROCKYTANKER <zyronegarciazleg@gmail.com>
2023-05-09 19:30:29 +0800

    Added loading screen

--- main.js
+++ main.js
@@ -11,4 +11,39 @@ document.addEventListener('mousemove', function(e){
 
     cursorinner.style.left = x + 'px';
     cursorinner.style.top = y + 'px';
+
+    cursor.style.opacity = '100%'
+    cursorinner.style.opacity = '100%'
 });
+
+document.addEventListener('mouseleave', function(){
+
+    cursor.style.opacity = '0%'
+    cursorinner.style.opacity = '0%'
+});
+
+// LOAD
+
+var body = document.body
+var Loading = document.querySelector('.Loading')
+
+function FinishLoading(){
+
+    document.querySelector('html').style.overflowY = 'scroll'
+    Loading.style.opacity = '0%'
+};
+
+document.addEventListener('load', () => {
+
+    console.log('The page is fully loaded.');
+    document.getElementById('LoadStatus').innerHTML = "Sucessfully loaded!"
+    setTimeout(FinishLoading, 2000)
+});
+
+// SKIP LOAD
+
+document.addEventListener('mousedown', function(){
+
+    document.getElementById('LoadStatus').innerHTML = "Loading skipped!"
+    setTimeout(FinishLoading, 2000)
+})
