05dd530820b14887a71716420f5088791e757c05
ROCKYTANKER <zyronegarciazleg@gmail.com>
2023-06-14 16:16:44 +0800

    mini change

--- Experimental/Test.js
+++ Experimental/Test.js
@@ -25,6 +25,7 @@ document.addEventListener('mouseleave', function(){
 // File upload
 
 var Output = document.querySelector(".Output")
+var OutputText = document.getElementById("OutputText")
 
 var UploadButton = document.getElementById("Upload")
 var UploadValue = ""
@@ -43,25 +44,35 @@ function FileUploaded(){
     if (file) {
 
         Output.innerHTML = "Invalid FILETYPE :( <br> The file type: " + file.type + " <br><br> ACCEPTED FILES are images... videos... audio.... and text files... !!! <br> - TankerJKT"
+        OutputText.innerHTML = "OUTPUT"
         
         if (file.type.match('text.*')) {
 
             reader.readAsText(file)
+            OutputText.innerHTML = "OUTPUT : Text : " + file.name
         } 
         
         if (file.type.match('video.*')) {
 
             Output.innerHTML = '<video src="' + URL.createObjectURL(file) + '" width=100% height=100% controls>'
+            OutputText.innerHTML = "OUTPUT : Video : " + file.name
         }
 
         if (file.type.match('audio.*')) {
 
             Output.innerHTML = '<audio src="' + URL.createObjectURL(file) + '" controls>'
+            OutputText.innerHTML = "OUTPUT : Audio : " + file.name
         }
 
         if (file.type.match('image.*')) {
 
-            Output.innerHTML = '<img src="' + URL.createObjectURL(file) + '" width=100% height=100%>'
+            Output.innerHTML = `<div height=100% width=100% style="background-image: url(` + URL.createObjectURL(file) + `); background-size: contain; background-repeat: no-repeat;"></div>`
+            OutputText.innerHTML = "OUTPUT : Image : " + file.name
         }
     }
+}
+
+if (document) {
+
+    console.log("iozxy is gayer: " + false)
 }
