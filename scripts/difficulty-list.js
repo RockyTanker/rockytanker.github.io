ef91be81466efeaca8dbd9ae32f802906ffc4514
ROCKYTANKER <zyronegarciazleg@gmail.com>
2026-01-20 01:12:54 +0800

    Relink system

--- scripts/difficulty-list.js
+++ scripts/difficulty-list.js
@@ -1,5 +1,5 @@
 // Initialization
-const url = 'https://raw.githubusercontent.com/RockyTanker/test-difficulty-list-db/refs/heads/main/List.md';
+const url = 'https://raw.githubusercontent.com/Ethan76167/TRIA.OS-Difficulty-List/refs/heads/main/migrated.md';
 var result, fetched
 var listArray = []
 
